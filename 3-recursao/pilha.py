d65ee97ad582acd25cdab837fcb7e0eca29f9a28
bruno <bpires.siqueira@gmail.com>
2024-12-12 20:36:58 -0300

    recursao completa

--- 3-recursao/pilha.py
+++ 3-recursao/pilha.py
@@ -20,3 +20,13 @@ def fat(x):
     return x * fat(x-1)
 
 print(fat(3))
+
+
+# RECAPITULANDO
+'''
+Recursão é quando uma função chama a si mesma.
+Toda função recursiva tem dois casos: o caso-base e o caso recursivo.
+Uma pilha tem duas operações: push e pop
+Todas as chamadas de função vão para a pilha de chamada. (CallStack)
+A pilha de chamada pode ficar muito grande e ocupar muita memória.
+'''
