cd6925222ee32efe6a430026465b46f2e34eceb1
bruno <bpires.siqueira@gmail.com>
2024-12-14 09:04:51 -0300

    fibonacci 1 e 2

--- 4-quicksort/fibonacci.py
+++ 4-quicksort/fibonacci.py
@@ -7,3 +7,18 @@ def fibonacci(position):
     return fibonacci(position - 1) + fibonacci(position - 2)
   
 print(fibonacci(6))
+
+# FUNCAO ITERATIVA
+
+def fibonnaci_iterativa(numero):
+  if numero <= 0:
+    return 0
+  if numero == 1:
+    return 1
+  
+  n1, n2 = 0, 1
+
+  for i in range(2, numero + 1):
+    n1, n2 = n2, n1 + n2
+  return n2
+print(fibonnaci_iterativa(7))
