5100feefb5c3986c3d68bc07fa19d4cd6e3dc5de
bruno <bpires.siqueira@gmail.com>
2024-12-17 11:18:35 -0300

    exercicios

--- 4-quicksort/exercicios.py
+++ 4-quicksort/exercicios.py
@@ -48,3 +48,13 @@ def busca_binaria_recursiva(lista, elemento, inicio, fim):
   else:
     #Caso recursivo, se o elemento nao for menor e sim maior, vai buscar da metade para a direita
     return busca_binaria_recursiva(lista, elemento, inicio, meio +1)
+
+# Introdução: Quanto tempo levaria, em notação Big O, para completar cada uma destas operações?
+# 4.5 Imprimir o valor de cada elemento em um array
+# Resposta: O(n)
+# 4.6 Duplicar o valor de cada elemento em um array
+# Resposta: O(n)
+# 4.7 Duplicar o valor apenas do primeiro elemento do array.
+# Resposta: O(1)
+# 4.8 Criar uma tabela de multiplicação com todos os elementos do array. Assim, caso o seu array seja [2, 3, 7, 8, 10], você primeiro multiplicará cada elemento por 2. Depois, multiplicará cada elemento por 3 e então por 7, e assim por diante.
+# Resposta: O(n²)
