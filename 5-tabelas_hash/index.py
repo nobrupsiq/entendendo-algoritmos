ef63ec1ba4c78bd33525827296af526348abc9e6
bruno <bpires.siqueira@gmail.com>
2024-12-19 22:26:32 -0300

    alguns exercicios e conteudo

--- 5-tabelas_hash/index.py
+++ 5-tabelas_hash/index.py
@@ -5,3 +5,27 @@ caderno['leite'] = 1.49
 caderno['abacate'] = 1.49
 
 print(caderno['maca'])
+
+'''
+EXERCICIOS
+Quais destas funções hash são consistentes?
+
+5.1 f(x) = 1
+Resposta: Consistente
+5.2 f(x) = rand()
+Resposta: Inconsistente
+5.3 f(x) = proximo_espaco_vazio()
+Resposta: Inconsistente
+5.4 f(x) = len(x)
+Resposta: Consistente
+'''
+
+# As tabelas hash são ótimas opções quando:
+# Você deseja mapear algum item com relação a outro
+# Você precisa pesquisar algo
+
+lista_telefonica = {}
+lista_telefonica['jenny'] = 8675309
+lista_telefonica['emergency'] = 911 
+
+print(lista_telefonica['jenny'])
